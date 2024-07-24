14d28ab78f95942256285a8ca3ed2655f8172866
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-07-23 21:19:33 -0700

    Initial commit

---
+++ src/main/java/com/skycatdev/autocut/Autocut.java
@@ -0,0 +1,15 @@
+package com.skycatdev.autocut;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Autocut implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("autocut");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/skycatdev/autocut/AutocutDataGenerator.java
@@ -0,0 +1,11 @@
+package com.skycatdev.autocut;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AutocutDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
