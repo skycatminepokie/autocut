e1696e0cd2ebb815b0bcb4bc9c3600a124d4642c
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-08-01 21:08:35 -0700

    Remove Timecode

--- src/client/java/com/skycatdev/autocut/Recorder.java
+++ src/client/java/com/skycatdev/autocut/Recorder.java
@@ -1,47 +1,39 @@
 package com.skycatdev.autocut;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class Recorder {
-    protected String title;
-    protected String recordingPath; // As opposed to the EDL output
-    protected String recordingName;
-    protected int fps;
     protected ArrayList<Clip> clips = new ArrayList<>();
+    protected ArrayList<RecordingEvent> events = new ArrayList<>();
 
-    public Recorder(String title, String recordingPath, String recordingName, int fps) {
-        this.title = title;
-        this.recordingPath = recordingPath;
-        this.recordingName = recordingName;
-        this.fps = fps;
+    public Recorder() {
     }
 
-    public File exportEdl() throws IOException { // TODO: Better throwing of errors
-        File edl = new File(recordingPath + ".edl");
-        assert !edl.exists() : "EDL already exists, and error handling has not been set up yet!"; // TODO
-        edl.createNewFile();
+    /*
+     public File exportEdl() throws IOException { // TODO: Better throwing of errors
+         File edl = new File(recordingPath + ".edl");
+         assert !edl.exists() : "EDL already exists, and error handling has not been set up yet!"; // TODO
+         edl.createNewFile();
 
-        try (PrintWriter pw = new PrintWriter(edl)) {
-            pw.println("TITLE: " + title);
-            pw.println("FCM: NON-DROP FRAME");
-            pw.println();
-            Timecode timelineIn = new Timecode(0, 0, 0, 0, fps);
-            Timecode timelineOut;
-            for (int i = 0; i < clips.size(); i++) {
-                // Clip header
-                Clip clip = clips.get(i);
-                timelineOut = timelineIn.add(clip.duration());
-                pw.printf("%03d  AX       V     C        %s %s %s %s\n", i + 1, clip.in().asString(), clip.out().asString(), timelineIn.asString(), timelineOut.asString());
-                timelineIn = timelineOut;
+         try (PrintWriter pw = new PrintWriter(edl)) {
+             pw.println("TITLE: " + title);
+             pw.println("FCM: NON-DROP FRAME");
+             pw.println();
+             long timelineIn = 0;
+             long timelineOut;
+             for (int i = 0; i < clips.size(); i++) {
+                 // Clip header
+                 Clip clip = clips.get(i);
+                 timelineOut = timelineIn + clip.duration();
+                 pw.printf("%03d  AX       V     C        %s %s %s %s\n", i + 1, clip.in().asString(), clip.out().asString(), timelineIn.asString(), timelineOut.asString());
+                 timelineIn = timelineOut;
 
-                pw.println("* FROM CLIP NAME: " + recordingName);
+                 pw.println("* FROM CLIP NAME: " + recordingName);
 
-                pw.println();
-            }
-        }
-        return edl; // TODO: Warn when there's over 999 clips
-    }
+                 pw.println();
+             }
+         }
+         return edl; // TODO: Warn when there's over 999 clips
+     }
+    */
 }
---
+++ src/client/java/com/skycatdev/autocut/RecordingEvent.java
@@ -0,0 +1,10 @@
+package com.skycatdev.autocut;
+
+public class RecordingEvent {
+    public long time;
+    public String description;
+
+    public RecordingEvent(long time, String description) {
+        this.time = time;
+    }
+}
