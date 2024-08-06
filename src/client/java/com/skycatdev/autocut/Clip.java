91d5f0a6e3eb7ec07a079ab4ed050a8103220fa5
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-08-05 21:03:12 -0700

    Generate filters

--- src/client/java/com/skycatdev/autocut/Clip.java
+++ src/client/java/com/skycatdev/autocut/Clip.java
@@ -1,7 +1,26 @@
 package com.skycatdev.autocut;
 
+/**
+ * A period of time in a recording
+ * @param in The timestamp to begin, in milliseconds
+ * @param out The timestamp to end, in milliseconds
+ */
 public record Clip(long in, long out) {
+    public Clip {
+        assert in < out;
+    }
+
     public long duration() {
         return out - in;
+    }
+
+    public String toBetweenStatement(String variable) {
+        double inSecs = (double) in / 1000;
+        double outSecs = (double) out / 1000;
+        return String.format("between(%s\\,%f\\,%f)", variable, inSecs, outSecs);
+    }
+
+    public Clip copy() {
+        return new Clip(in, out);
     }
 }
--- src/client/java/com/skycatdev/autocut/Recorder.java
+++ src/client/java/com/skycatdev/autocut/Recorder.java
@@ -5,7 +5,8 @@ import org.bytedeco.javacv.*;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.stream.Stream;
+import java.util.Collection;
+import java.util.Comparator;
 
 public class Recorder {
     protected ArrayList<Clip> clips = new ArrayList<>();
@@ -17,16 +18,70 @@ public class Recorder {
     public void onRecordingEnded(String outputPath) {
         FFmpegLogCallback.set();
         File recording = new File(outputPath);
-        File export = recording.toPath().resolveSibling("cut" + recording.getName()).toFile();
+        File export = recording.toPath().resolveSibling("cut" + recording.getName()).toFile(); // Successful cut: ffmpeg -i '.\cut2024-08-02 21-46-13.mkv' -vf trim=1:2 -af atrim=1:2 output.mkv
         String ffmpeg = Loader.load(org.bytedeco.ffmpeg.ffmpeg.class);
-        ProcessBuilder pb = new ProcessBuilder(ffmpeg, "-f", "concat", "-safe", "0", "-i", "C:\\Users\\USER\\Videos\\Recordings\\texxt.txt", "-c", "copy", export.getAbsolutePath());
+        File[] filters;
         try {
-            pb.inheritIO().start().waitFor();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+           filters = buildFilters(clips);
+           ProcessBuilder pb = new ProcessBuilder(ffmpeg, "-safe", "0", "-i", recording.getAbsolutePath(), "-vf", filters[0].getAbsolutePath(), "-af", filters[1].getAbsolutePath(), export.getAbsolutePath());
+           pb.inheritIO().start().waitFor();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        return;
+    }
+
+    /**
+     * Create filters to keep only the given clips in.
+     * @param clips At least one clip. Clips may overlap.
+     * @return {@code new File[] {selectFilter, aselectFilter}}
+     * @throws IOException
+     */
+    // Yes the return for the javadoc is exactly the return line, but it doesn't get much clearer than that line.
+    public static File[] buildFilters(Collection<Clip> clips) throws IOException { // Find a faster way? Let me know!
+        if (clips.isEmpty()) {
+            throw new IllegalArgumentException("clips.isEmpty(), cannot build a (meaningful) filter out of no clips.");
+        }
+        var mergedClips = mergeClips(clips);
+        File selectFilter = File.createTempFile("autocutSelectFilter", null);
+        selectFilter.deleteOnExit();
+        File aselectFilter = File.createTempFile("autocutAselectFilter", null);
+        aselectFilter.deleteOnExit();
+        try (PrintWriter selectWriter = new PrintWriter(selectFilter); PrintWriter aselectWriter = new PrintWriter(aselectFilter)) {
+            String between = mergedClips.getFirst().toBetweenStatement("t");
+            selectWriter.print("select='" + between);
+            aselectWriter.print("aselect='" + between);
+            for (int i = 1; i < mergedClips.size(); i++) {
+                between = mergedClips.get(i).toBetweenStatement("t");
+                selectWriter.print("+" + between);
+                aselectWriter.print("+" + between);
+            }
+            selectWriter.print("'");
+            aselectWriter.print("'");
+        }
+        return new File[] {selectFilter, aselectFilter};
+    }
+
+    /**
+     * Sort and merge all overlapping clips together.
+     * @param clips The clips to merge together
+     * @return A new ArrayList of merged clips.
+     */
+    private static ArrayList<Clip> mergeClips(Collection<Clip> clips) {
+        ArrayList<Clip> mergedClips = new ArrayList<>(clips.stream().map(Clip::copy).sorted(Comparator.comparing(Clip::in)).toList()); // New list so that it's mutable
+        int i = 0;
+        while (i < mergedClips.size() - 1) { // Don't try to merge the last clip, there's nothing to merge it with
+            Clip current = mergedClips.get(i);
+            Clip next = mergedClips.get(i + 1);
+            if (current.out() <= next.in()) { // If current overlaps next
+                Clip newClip = new Clip(current.in(), Math.max(current.out(), next.out())); // Take the union
+                mergedClips.set(i, newClip); // Replace the current
+                mergedClips.remove(i + 1); // Yeet the next, it's been combined
+                continue; // And check this clip for union with the next
+            }
+            i ++; // This clip has no overlap, try the next one
+        }
+        return mergedClips;
     }
 }
