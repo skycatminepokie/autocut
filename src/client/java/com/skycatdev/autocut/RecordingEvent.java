185f1b0cc1a148e2b101c3c2e24471fa707ca1f5
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-08-20 16:03:33 -0700

    Add type to RecordingEvent

--- src/client/java/com/skycatdev/autocut/RecordingEvent.java
+++ src/client/java/com/skycatdev/autocut/RecordingEvent.java
@@ -1,10 +1,15 @@
 package com.skycatdev.autocut;
 
+import net.minecraft.util.Identifier;
+
 public class RecordingEvent {
     public long time;
+    public Identifier type;
     public String description;
 
-    public RecordingEvent(long time, String description) {
+    public RecordingEvent(long time, Identifier type, String description) {
         this.time = time;
+        this.type = type;
+        this.description = description;
     }
 }
