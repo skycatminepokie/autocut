160b9d0baa139ba016bf2bfb6ed71954d81b7638
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-08-27 16:21:24 -0700

    Add extra data

--- src/client/java/com/skycatdev/autocut/AutocutClient.java
+++ src/client/java/com/skycatdev/autocut/AutocutClient.java
@@ -1,5 +1,6 @@
 package com.skycatdev.autocut;
 
+import com.skycatdev.autocut.clips.Clip;
 import com.skycatdev.autocut.clips.ClipBuilder;
 import com.skycatdev.autocut.clips.ClipTypes;
 import io.obswebsocket.community.client.OBSRemoteController;
@@ -7,7 +8,9 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.client.player.ClientPlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.Nullable;
 
 import java.sql.SQLException;
@@ -24,20 +27,28 @@ public class AutocutClient implements ClientModInitializer {
                 long time = System.currentTimeMillis();
                 try {
                     currentRecordingHandler.addClip(new ClipBuilder(time - 250, time, time + 250, ClipTypes.BREAK_BLOCK)
-                            .setDescription("Broke " + state.getBlock().getName().toString())
-                            .build()); // TODO: Localize TODO: MORE DATA
+                            .setDescription("Broke " + state.getBlock().getName().getString())
+                            .setObject(Registries.BLOCK.getId(state.getBlock()).toString())
+                            .setObjectLocation(Vec3d.of(pos))
+                            .setSource(player.getNameForScoreboard())
+                            .setSourceLocation(player.getPos())
+                            .build()); // TODO: Localize
                 } catch (SQLException e) { // TODO
                     e.printStackTrace();
                 }
             }
         }));
         AttackEntityCallback.EVENT.register(((player, world, hand, entity, hitResult) -> {
-            if (currentRecordingHandler != null && entity != null) {
+            if (currentRecordingHandler != null && entity != null) { // WARN: DEBUG Ignores when not attacking entity
                 long time = System.currentTimeMillis();
                 try {
-                    currentRecordingHandler.addClip(new ClipBuilder(time - 250, time, time + 250, ClipTypes.ATTACK_ENTITY)
+                    ClipBuilder builder = new ClipBuilder(time - 250, time, time + 250, ClipTypes.ATTACK_ENTITY)
                             .setDescription("Attacked " + entity.getName())
-                            .build()); // TODO: More data
+                            .setSource(player.getNameForScoreboard())
+                            .setSourceLocation(player.getPos())
+                            .setObject(entity.getNameForScoreboard())
+                            .setObjectLocation(entity.getPos());
+                    currentRecordingHandler.addClip(builder.build());
                 } catch (SQLException ignored) { // TODO
                 }
             }
