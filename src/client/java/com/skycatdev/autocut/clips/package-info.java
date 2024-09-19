fc2b7fa37c9cc35abc6690ba050b5615551b358a
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-09-19 14:50:02 -0700

    Update javadocs

--- src/client/java/com/skycatdev/autocut/clips/package-info.java
+++ src/client/java/com/skycatdev/autocut/clips/package-info.java
@@ -6,7 +6,7 @@
  * 4. Fill in required overrides<br>
  * 5. Register it at startup with {@link com.skycatdev.autocut.clips.ClipTypes#registerClipType(net.minecraft.util.Identifier, com.mojang.serialization.Codec, java.util.function.Supplier)}<br>
  * 6. Create a clip when the time is right! Make sure to check if it's enabled first.<br>
- * @see com.skycatdev.autocut.clips.ShootPlayerClipType for a simple example
- * @see com.skycatdev.autocut.clips.TakeDamageClipType for a slightly more complex example (extra config)
+ * @see com.skycatdev.autocut.clips.ShootPlayerClipType ShootPlayerClipType for a simple example
+ * @see com.skycatdev.autocut.clips.TakeDamageClipType TakeDamageClipType for a slightly more complex example (extra config)
  */
 package com.skycatdev.autocut.clips;
