3d49c3d3a8424ac2094e3eede7ded4b6da4903db
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-09-21 20:38:19 -0700

    Fix log message

--- src/client/java/com/skycatdev/autocut/AutocutClient.java
+++ src/client/java/com/skycatdev/autocut/AutocutClient.java
@@ -71,7 +71,7 @@ public class AutocutClient implements ClientModInitializer {
                     try {
                         currentRecordingManager.addClip(MANUAL.clipType().createClip(time));
                     } catch (SQLException e) {
-                        Autocut.LOGGER.warn("Unable to store use item event", e);
+                        Autocut.LOGGER.warn("Unable to store use manual clip", e);
                     }
                 }
             }
