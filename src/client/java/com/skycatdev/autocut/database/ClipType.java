0c16b561fed00de4e254f8e872f74773c2c51120
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2025-01-15 15:12:19 -0700

    New ClipType

---
+++ src/client/java/com/skycatdev/autocut/database/ClipType.java
@@ -0,0 +1,88 @@
+package com.skycatdev.autocut.database;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.skycatdev.autocut.config.ExportGroupingMode;
+import com.skycatdev.autocut.record.RecordingTrigger;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
+
+public class ClipType {
+	public static final Codec<ClipType> CODEC = RecordCodecBuilder.create((instance) -> instance.group(
+			RecordingTrigger.CODEC.fieldOf("start_trigger").forGetter(ClipType::getStartTrigger),
+			RecordingTrigger.CODEC.optionalFieldOf("end_trigger").forGetter((clipType) -> Optional.ofNullable(clipType.getEndTrigger())),
+			Codec.LONG.fieldOf("start_offset").forGetter(ClipType::getStartOffset),
+			Codec.LONG.fieldOf("end_offset").forGetter(ClipType::getEndOffset),
+			Codec.BOOL.fieldOf("enabled").forGetter(ClipType::isEnabled),
+			Codec.BOOL.fieldOf("inverted").forGetter(ClipType::isInverted),
+			Identifier.CODEC.comapFlatMap((id) -> {
+				@Nullable ExportGroupingMode groupingMode = ExportGroupingMode.fromId(id);
+				return groupingMode != null ? DataResult.success(groupingMode) : DataResult.error(() -> "Not a valid ExportGroupingMode: " + id);
+			}, ExportGroupingMode::getId).fieldOf("group_mode").forGetter(ClipType::getExportGroupingMode)
+	).apply(instance, ClipType::new));
+
+	protected RecordingTrigger startTrigger;
+	protected @Nullable RecordingTrigger endTrigger;
+	protected long startOffset;
+	protected long endOffset;
+	protected boolean enabled;
+	protected boolean inverted;
+	protected ExportGroupingMode exportGroupingMode;
+
+	public ClipType(RecordingTrigger startTrigger, @Nullable RecordingTrigger endTrigger, long startOffset, long endOffset, boolean enabled, boolean inverted, ExportGroupingMode exportGroupingMode) {
+		this.startTrigger = startTrigger;
+		this.endTrigger = endTrigger;
+		this.startOffset = startOffset;
+		this.endOffset = endOffset;
+		this.enabled = enabled;
+		this.inverted = inverted;
+		this.exportGroupingMode = exportGroupingMode;
+	}
+
+	public ClipType(RecordingTrigger startTrigger,
+					@SuppressWarnings("OptionalUsedAsFieldOrParameterType") Optional<RecordingTrigger> endTrigger,
+					long startOffset,
+					long endOffset,
+					boolean enabled,
+					boolean inverted,
+					ExportGroupingMode exportGroupingMode) {
+		this.startTrigger = startTrigger;
+		this.endTrigger = endTrigger.orElse(null);
+		this.startOffset = startOffset;
+		this.endOffset = endOffset;
+		this.enabled = enabled;
+		this.inverted = inverted;
+		this.exportGroupingMode = exportGroupingMode;
+	}
+
+	public long getEndOffset() {
+		return endOffset;
+	}
+
+	public @Nullable RecordingTrigger getEndTrigger() {
+		return endTrigger;
+	}
+
+	public ExportGroupingMode getExportGroupingMode() {
+		return exportGroupingMode;
+	}
+
+	public long getStartOffset() {
+		return startOffset;
+	}
+
+	public RecordingTrigger getStartTrigger() {
+		return startTrigger;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public boolean isInverted() {
+		return inverted;
+	}
+}
--- src/client/java/com/skycatdev/autocut/record/RecordingTrigger.java
+++ src/client/java/com/skycatdev/autocut/record/RecordingTrigger.java
@@ -1,9 +1,11 @@
 package com.skycatdev.autocut.record;
 
+import com.mojang.serialization.Codec;
 import net.minecraft.util.Identifier;
 
 // TODO
 public class RecordingTrigger {
+    public static final Codec<RecordingTrigger> CODEC = null; // TODO
     protected Identifier id;
 
     public Identifier getId() {
