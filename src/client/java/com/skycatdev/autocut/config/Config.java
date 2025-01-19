179aa9514a6b4bcfaa07e61f396b6baa647a0672
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2025-01-18 21:18:17 -0700

    Handle same triggers

--- src/client/java/com/skycatdev/autocut/config/Config.java
+++ src/client/java/com/skycatdev/autocut/config/Config.java
@@ -1,7 +1,6 @@
 package com.skycatdev.autocut.config;
 
 import com.skycatdev.autocut.database.ClipType;
-import com.skycatdev.autocut.trigger.RecordingTriggers;
 import net.fabricmc.loader.api.FabricLoader;
 import org.jetbrains.annotations.Nullable;
 
@@ -48,7 +47,8 @@ public class Config {
         } else {
             clipTypes = new ArrayList<>();
             // clipTypes.add(new ClipType(RecordingTriggers.MANUAL_TRIGGER, null, 30000, 100, true, false, ExportGroupingMode.INDIVIDUAL));
-            clipTypes.add(new ClipType(RecordingTriggers.MANUAL_TRIGGER, RecordingTriggers.ATTACK_ENTITY_TRIGGER, 100, 100, true, false, ExportGroupingMode.INDIVIDUAL));
+            // clipTypes.add(new ClipType(RecordingTriggers.MANUAL_TRIGGER, RecordingTriggers.ATTACK_ENTITY_TRIGGER, 100, 100, true, false, ExportGroupingMode.INDIVIDUAL));
+            // clipTypes.add(new ClipType(RecordingTriggers.MANUAL_TRIGGER, RecordingTriggers.MANUAL_TRIGGER, 100, 100, true, false, ExportGroupingMode.INDIVIDUAL));
             // TODO: add defaults
         }
         return new Config(clipTypes, ExportConfig.readOrDefault(EXPORT_CONFIG_FILE));
